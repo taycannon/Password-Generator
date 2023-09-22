6adf7a936384e4e317016d545729a8536e8a1100
taycannon <taycannon2003@gmail.com>
2023-09-21 20:00:57 -0500

    Added Uppercase as well as final Passwordwith push

--- script.js
+++ script.js
@@ -1,33 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+//Created userChoice, lowercase, and uppercase
+var userChoice = [];
+let finalPassword = [];
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase ="ABCDEFGHIGKLMNOPQRSTUVWXYZ"
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+passwordText.value = password;
+
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 function generatePassword(){
-  let passwordLength = prompt('Please provide a minimun length of 8 and a maxium length of 128 for your perferred password!');
-  console.log('password length --> ', passwordLength);
-  if(passwordLength < 8 || passwordLength > 128){
-    alert(['please provide the requested length'])
-  }
-  else {
-    var includeLowercase = confirm("Would you like to include Lowercase letters")
-    if(includeLowercase){
-      console.log('lower case were chosen',Lowercase);
-    }
-  }
+let passwordLength = prompt('please provide a minimum length of 8 and a maximum length of 128 for your preferred password');
+console.log('password length --> ', passwordLength);
+if(passwordLength < 8 || passwordLength > 128){
+// alerts users to try again and did not finish condition
+alert(['please provide the requested length'])
 }
+else {
+//Lowercase Code
+var includelowerCase = confirm("Would you want to include Lowercase letters");
+if(includelowerCase){
+//push means to add new items at the end of a array
+userChoice.push(...lowerCase)
+}
+
+//Uppercase Code
+var includeupperCase = confirm("Would you like Uppercase");
+if(includeupperCase) {
+userChoice.push(...upperCase)
+}
+
+//Numeric Code
+var includeNumeric
+
+
+
+console.log('user choice array --> ', userChoice);
+
+//This is the loop
+for (let i = 0; i < passwordLength; i++) {
+//Math floor brings any numbers that is lower numbers & random gives a random reponse
+finalPassword.push(userChoice[Math.floor(Math.random() * userChoice.length)])
+console.log('finalPassword --> ', finalPassword);
+}
+}
+}
+
 
 
 
